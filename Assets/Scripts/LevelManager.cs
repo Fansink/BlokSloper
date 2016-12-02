6216632d5c2a8e300f8d50068f4693b1880d99d9
Fansink <flansink@kza.nl>
2016-12-02 07:17:47 +0100

    Eerst commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void StartGame(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame(string name)
+    {
+        Application.Quit();
+    }
+
+    public void WinGame(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+}
---
+++ Assets/Scripts/MuziekSpeler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuziekSpeler : MonoBehaviour {
+
+    static MuziekSpeler instance = null;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+        
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+}
