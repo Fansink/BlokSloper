199d0fb20d17f8e749d8f89ed5f9e5516ed2bb99
Fansink <flansink@kza.nl>
2017-01-04 17:41:57 +0100

    Ball script toegevoegd
    Hij wordt vastgezet op de paddle en met een muisklik weggeschoten

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    public Rigidbody2D rb;
+
+    private bool hasStarted = false;
+    private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle.transform.position;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            // De bal wordt vergezet op de paddle
+            transform.position = paddle.transform.position + paddleToBallVector;
+            
+            // Wachten op de muisklik
+            if (Input.GetMouseButtonDown(0))
+            {
+                print("Pressed left click, bal afvuren");
+                hasStarted = true;
+                rb.velocity = new Vector2 (2f, 10f);
+            }
+        }
+    }
+}
